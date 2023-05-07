96d964cedde7c1f26f6953539b10fc991227edac
annusingh2403 <annusingh2403@gmail.com>
2023-05-07 17:01:26 +0530

    added active nav functionality in navbar

--- src/components/home/navbar/Navbar.js
+++ src/components/home/navbar/Navbar.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+
+    const [nav, setNav] = useState('home');
+
+    const activeNav = (e, param) => {
+        setNav(param);
+    }
 
   return (
 
@@ -15,11 +21,11 @@ const Navbar = () => {
                 <p>♪ The little coffee shop on the corner of the street ♥</p>
             </h3>
             <ul className='nav-list'>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/products" >Facts</Link></li>
-                <li><Link to="/gallery">Gallery</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/" className={nav === "home" ? "activeNav" : ''} onClick={((e) => activeNav(e, "home"))}>Home</Link></li>
+                <li><Link to="/products" className={nav === "facts" ? "activeNav" : ''} onClick={((e) => activeNav(e, "facts"))}>Facts</Link></li>
+                <li><Link to="/gallery" className={nav === "gallery" ? "activeNav" : ''} onClick={((e) => activeNav(e, "gallery"))}>Gallery</Link></li>
+                <li><Link to="/about" className={nav === "about" ? "activeNav" : ''} onClick={((e) => activeNav(e, "about"))}>About</Link></li>
+                <li><Link to="/contact" className={nav === "contact" ? "activeNav" : ''} onClick={((e) => activeNav(e, "contact"))}>Contact</Link></li>
             </ul>
         </nav>
     </div>
