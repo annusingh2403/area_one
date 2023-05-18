b234564b1ef8cdd839daeb52ba199df853bb38ba
annusingh2403 <annusingh2403@gmail.com>
2023-05-18 10:25:54 +0530

    added gallery head in gallery.js

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React from 'react';
+import './Gallery.css';
+import GimageOne from '../../images/gimage-one.jpg';
+import GimageTwo from '../../images/gimage-two.jpg';
+import GimageThree from '../../images/gimage-three.jpg';
+import GimageFour from '../../images/gimage-four.jpg';
+import GimageFive from '../../images/gimage-five.jpg';
+import GimageSix from '../../images/gimage-six.jpg';
+import GimageSeven from '../../images/gimage-seven.jpg';
 
 const Gallery = () => {
   return (
-    <div>
-      Gallery
-    </div>
+    <>
+        <div className="gallery-head">
+            <div className="gimage-one">
+                <img src={GimageOne} alt="" />
+            </div>
+            <div className="gimage-two">
+                <img src={GimageTwo} alt="" />
+            </div>
+            <div className="gimage-three">
+                <img src={GimageThree} alt="" />
+            </div>
+            <div className="gimage-four">
+                <img src={GimageFour} alt="" />
+            </div>
+            <div className="gimage-five">
+                <img src={GimageFive} alt="" />
+            </div>
+            <div className="gimage-six">
+                <img src={GimageSix} alt="" />
+            </div>
+            <div className="gimage-seven">
+                <img src={GimageSeven} alt="" />
+            </div>
+        </div>
+        <div>
+            
+        </div>
+    </>
   )
 }
 
