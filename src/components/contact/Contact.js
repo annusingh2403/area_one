043c12fae373ecd8e2787ffdb5a3bdc10c3074a3
annusingh2403 <annusingh2403@gmail.com>
2023-05-27 10:13:07 +0530

    tab functionality done

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Contact.css';
 
 const Contact = () => {
+
+    const [contact, setContact] = useState(true);
+    const [subscribe, setSubscribe] = useState(false);
+
+    const Contact = () => {
+        setContact(true);
+        setSubscribe(false);
+    }
+
+    const Subscribe = () => {
+        setSubscribe(true);
+        setContact(false)
+    }
+
   return (
     <>
         <div className='contact'>
-            <div className="contact-box">
-                <form>
+
+            <p className={contact ? "active-button" : 'contact-button'} onClick={Contact}>Contact</p>
+            <p className={subscribe ? "active-button" : 'contact-button'} onClick={Subscribe}>Subscribe</p>
+
+            {contact ? 
+                <div className="contact-box">
+                    <form>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" />
+                        <label htmlFor=""></label>
+                    </form>
+                </div> : ''
+            }
+
+            {subscribe ? 
+                <div>
                     <label htmlFor="name">Name</label>
-                    <input type="text" />
-                    <label htmlFor=""></label>
-                </form>
-            </div>
+                    <input type="text" name='name'/>
+                    <label htmlFor="email">Email</label>
+                    <input type="text" name='email'/>
+                    <button>Subscribe</button>
+                </div> : ''
+            }
+            
         </div>   
     </>
   )
