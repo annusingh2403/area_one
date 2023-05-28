b47308215050cdb69e25e20b4d799b6e0d134da9
annu singh <annusingh2403@gmail.com>
2023-05-28 20:41:53 +0530

    contact.js completed

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -20,35 +20,35 @@ const Contact = () => {
     <>
         <div className='contact'>
             <div className="contact-main">
-            <div className="button-box">
-                <p className={contact ? "active-button" : 'contact-button'} onClick={Contact}>Contact</p>
-                <p className={subscribe ? "active-button" : 'contact-button'} onClick={Subscribe}>Subscribe</p>
-            </div>
-           
-            {contact ? 
-                <div className="contact-box">
-                    <p>Name: Annu Singh Chauhan</p>
-                    <p>Mobile: 7380698894</p>
-                    <address>Address: A-19, Vidyut Nagar, Vaishali Nagar, Jaipur-302021</address>
-                </div> : ''
-            }
+                <div className="button-box">
+                    <p className={contact ? "active-button" : 'contact-button'} onClick={Contact}>Contact</p>
+                    <p className={subscribe ? "active-button" : 'contact-button'} onClick={Subscribe}>Subscribe</p>
+                </div>
+            
+                {contact ? 
+                    <div className="contact-box">
+                        <p>Name: Annu Singh Chauhan</p>
+                        <p>Mobile: 7380698894</p>
+                        <address>Address: A-19, Vidyut Nagar, Vaishali Nagar, Jaipur-302021</address>
+                    </div> : ''
+                }
 
-            {subscribe ? 
-                <div className='subscribe-box'>
-                    <div>
-                        <input type="text" placeholder='Name:'/>
-                    </div>
-                    <div>
-                        <input type="email" placeholder='Email:'/>
-                    </div>
-                    <div>
-                        <input type="number" placeholder='Mobile:'/>
-                    </div>
-                    <div className='subscribe-button'>
-                        <button>Submit</button>
-                    </div>
-                </div> : ''
-            }
+                {subscribe ? 
+                    <div className='subscribe-box'>
+                        <div>
+                            <input type="text" placeholder='Name:'/>
+                        </div>
+                        <div>
+                            <input type="email" placeholder='Email:'/>
+                        </div>
+                        <div>
+                            <input type="number" placeholder='Mobile:'/>
+                        </div>
+                        <div className='subscribe-button'>
+                            <button>Submit</button>
+                        </div>
+                    </div> : ''
+                }
             </div>
         </div>   
     </>
